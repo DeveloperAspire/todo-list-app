989f4f97fe17b65d8c8818182f07ebff82bad766
Franklin <franklinokolie86@gmail.com>
2021-02-12 12:05:22 +0100

    Feature: Implemented Local Storage

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ const taskList = document.querySelector('#tasklist');
 
 
 
+// ONLOAD THIS GETS THE DATA FROM LOCAL STORAGE AND RENDER TO UI
 
-// THIS FUNCTION ADDS TASK, VALIDATE INPUT AND ALSO DELETE IT
-function addElement(){
+// DOM load event
+document.addEventListener('DOMContentLoaded', showTasks)
 
+function showTasks(){
+  let todos
+  if(localStorage.getItem('todos' === null)){
+    todos = []
+  } else{
+    todos = JSON.parse(localStorage.getItem('todos'))
+  }
+  todos.forEach((todo)=>{
     // THIS CREATES THE LIST OF TASK
     const childList = document.createElement('li');
     childList.classList.add('check');
@@ -21,13 +30,11 @@ function addElement(){
     // CREATED INPUT AND SET VALUE
     const checkedItem = document.createElement('input');
     checkedItem.setAttribute("type", "checkbox");
-    
-
+  
     // CREATED SPAN AND IT TEXT
     const span = document.createElement('span')
     span.classList.add('ml-2')
-    span.textContent= input.value;
-
+    span.textContent= todo;
 
     // APPEND VALUE TO DIV
     checkedDiv.appendChild(checkedItem)
@@ -45,33 +52,65 @@ function addElement(){
     taskSection.classList.remove('hidden')
     taskSection.appendChild(childList)
 
-     checkValidation(input.value, childList)
+  })
+
+}
+
+
+// ADD TASK TO THE TODO LIST
+function addElement(){
+
+    // THIS CREATES THE LIST OF TASK
+    const childList = document.createElement('li');
+    childList.classList.add('check');
+
+    // CREATED DIV TO HOLD INPUT AND SPAN
+    const checkedDiv= document.createElement('div');
+
+    // CREATED INPUT AND SET VALUE
+    const checkedItem = document.createElement('input');
+    checkedItem.setAttribute("type", "checkbox");
+  
+    // CREATED SPAN AND IT TEXT
+    const span = document.createElement('span')
+    span.classList.add('ml-2')
+    span.textContent= input.value;
+
+    // APPEND VALUE TO DIV
+    checkedDiv.appendChild(checkedItem)
+    checkedDiv.appendChild(span)
+
+    // CREATED ICON
+    const icon = document.createElement('i')
+    icon.classList.add('far', 'fa-trash-alt')
+
+    // APPEND DIV AND ICON TO THE LIST OF TASK
+    childList.appendChild(checkedDiv);
+    childList.appendChild(icon);
+
+    // RENDER TO HTML PAGE
+    taskSection.classList.remove('hidden')
+    taskSection.appendChild(childList)
 
     // CLEAR INPUT
-
     setTimeout(function clearInput(){
         input.value=""
     }, 500)
 
-    // IF DELETE ICON  IS CLICKED
 
-    icon.addEventListener('click', removetaskList);
+    // VALIDATE FOR EMPTY INPUT
+    checkValidation(input.value, childList)
 
-    // THIS FUNCTION ACTS FOR REMOVING TASK
-
-  function removetaskList(e) {
-
- taskSection.removeChild(childList)
-
-
-  }
+    // STORE TASK TO LOCAL STORAGE
+   storeToLocalStorage(input.value)
 
 
 
 
 }
 
-// VALIDATION
+
+// VALIDATION THE INPUT FOR EMPTY INPUATION
 
 function checkValidation(inputData, childList){
   if(inputData == ""){
@@ -87,7 +126,52 @@ function checkValidation(inputData, childList){
 
 }
 
+// DELETE TASK FROM THE TODO LIST
+function deleteTask(e){
+  task = e.target
+  if(task.classList.contains('far')){
+    taskSection.removeChild(task.parentElement)
 
+    // DELETE FROM LOCAL STORAGE
+    deleteTaskFromLocalStorage(task.parentElement)
+
+  }
+}
+
+// STORE TO LOCAL STORAGE
+function storeToLocalStorage(todo){
+  let todos;
+
+  if(localStorage.getItem('todos') === null){
+    todos=[]
+  } else{
+    todos= JSON.parse(localStorage.getItem('todos'))
+  }
+
+  todos.push(todo)
+
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// DELETE FROM LOCAL STORAGE WHEN USER DELETES
+ function deleteTaskFromLocalStorage(todo) {
+  let todos;
+
+  if(localStorage.getItem('todos') === null){
+    todos=[]
+  } else{
+    todos= JSON.parse(localStorage.getItem('todos'))
+  }
+
+  todos.forEach((todoItem, index)=>{
+    if(todoItem === todo.textContent){
+      todos.splice(index, 1)
+    }
+  })
+
+  localStorage.setItem('todos', JSON.stringify(todos))
+   
+ }
 
 
 
@@ -100,7 +184,6 @@ function checkValidation(inputData, childList){
 loadEventListener()
 
 function loadEventListener() {
-  
 
 button.addEventListener('click',addElement)
  taskSection.addEventListener('click', deleteTask)
